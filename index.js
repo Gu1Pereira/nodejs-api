68d6f384f08dd99b8c8d181d5560ae7dc7921e10
GUILHERME PEREIRA <guilhermemap85@gmail.com>
2024-06-24 21:02:13 -0300

    listagem
    lista de heroes com node

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const app = express()
 
-app.get("/", function(req, res){
-    res.send("Hello World")
+const herois = ["homem aranha", "hulk", "ms. marvel"]
+
+//ENDPOINT Listagem de todos os herois (READ ALL => [GET])
+app.get("/heroes", function(req, res){
+    res.send(herois)
 })
 
-app.get("/ptbr", function(req, res){
-    res.send("Olá Mundo")
+
+app.get("/heroes/:id", function(req, res){
+    //acessar o parametro de rota ID
+    const id = req.params.id
+
+    // pego o item no array
+    const umheroi = herois[id]
+
+    //envio o item encontrado como resposta
+    res.send(umheroi)
 })
 
-app.post()
-app.put()
-app.delete()
+// app.post()
+// app.put()
+// app.delete()
 
 app.listen(3000)
