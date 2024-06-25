87a4fd5c4d00094b132cc3df9fb59f7c6c1345b9
GUILHERME PEREIRA <guilhermemap85@gmail.com>
2024-06-24 21:27:49 -0300

    listagem
    node

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/heroes", function(req, res){
     res.send(herois)
 })
 
-
+// ENDPOINT listagem de UM herói (READ SINGLE => [GET])
 app.get("/heroes/:id", function(req, res){
     //acessar o parametro de rota ID
     const id = req.params.id
@@ -20,8 +20,16 @@ app.get("/heroes/:id", function(req, res){
     res.send(umheroi)
 })
 
-// app.post()
-// app.put()
-// app.delete()
+// todo o corpo da requisição virá como JSON
+app.use(express.json())
+
+// ENDPOINT de criação (CREATE => [POST])
+app.post("/heroes", function(req, res){
+    res.send(req.body)
+})
+
+// app.post() - 
+// app.put() -
+// app.delete() - 
 
 app.listen(3000)
